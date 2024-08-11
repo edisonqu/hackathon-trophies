95b27b1747f4bf5a52c81d1d85776c64a6d97163
Rachel Chen <rchen457@uwo.ca>
2024-08-11 05:03:15 -0400

    add trophy logo

--- client/src/components/nav.tsx
+++ client/src/components/nav.tsx
@@ -1,7 +1,7 @@
 const Nav = () => {
   return (
-    <div className="fixed bottom-10 w-screen grid place-items-center min-h-20 z-10">
-      <div className="bg-black bg-opacity-50 text-white p-4 rounded-md flex items-center shadow-lg">
+    <div className="fixed bottom-10 w-screen grid place-items-center min-h-20 z-10 pointer-events-none">
+      <div className="bg-black bg-opacity-50 text-white p-4 rounded-md flex items-center shadow-lg pointer-events-auto">
         <div className="flex gap-4 items-center">
           <button className="bg-white text-gray-950 px-4 py-2 rounded-md flex items-center justify-center">
             <span className="mr-2">🏠</span>{" "}
