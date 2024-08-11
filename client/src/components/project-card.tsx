6887363001592eb3e27b4ed5cdee81687baa80d8
Rachel Chen <rchen457@uwo.ca>
2024-08-11 02:12:45 -0400

    profile and home pg

---
+++ client/src/components/project-card.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import Badge from "@/components/badge";
+import { useRouter } from "next/navigation";
+import React from "react";
+
+interface ProjectCardProps {
+  id: string;
+  color: string;
+}
+
+const ProjectCard: React.FC<ProjectCardProps> = ({ id, color }) => {
+  const router = useRouter();
+
+  return (
+    <div
+      className={`bg-${color}-400 p-4 rounded-lg shadow-lg`}
+      onClick={() => router.push("/" + id)}
+    >
+      <Badge />
+    </div>
+  );
+};
+
+export default ProjectCard;
